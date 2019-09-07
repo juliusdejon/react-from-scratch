ac026b2490cf95b38fd17dc764f4c411b082a4d9
Julius Dejon <juliusdejon@gmail.com>
2019-09-07 15:48:52 +0800

    Styling

--- src/js/HomePage/homepage.js
+++ src/js/HomePage/homepage.js
@@ -1,36 +1,30 @@
 import React, { Component } from "react";
 
-import Input from "../components/generic-input";
-
 import logo from "../../img/reactJS.gif";
-
 import "./homepage.scss";
+
 import GenericInput from "../components/generic-input";
 
 class HomePage extends Component {
   constructor() {
     super();
     this.state = {
-      seo_title: ""
+      name: ""
     };
-    this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event) {
     this.setState({ [event.target.id]: event.target.value });
   }
   render() {
-    const { seo_title } = this.state;
+    const { name } = this.state;
     return (
       <article>
         <img src={logo} alt="react-git" className="logo" />
         <form id="article-form">
           <GenericInput
-            text="SEO title"
-            label="seo_title"
-            type="text"
-            id="seo_title"
-            value={seo_title}
-            placeholder={"Enter SEO"}
+            id="name"
+            value={name}
+            placeholder={"I'm a Generic Input"}
             handleChange={this.handleChange}
           />
         </form>
--- src/js/components/generic-input.js
+++ src/js/components/generic-input.js
@@ -2,33 +2,20 @@ import React from "react";
 import { Input } from "semantic-ui-react";
 import PropTypes from "prop-types";
 
-const GenericInput = ({
-  label,
-  text,
-  type,
-  id,
-  value,
-  placeholder,
-  handleChange
-}) => (
-  <div className="form-group">
-    <label htmlFor={label}>{text}</label>
-    <Input
-      focus
-      placeholder={placeholder}
-      type={type}
-      className="ui focus input"
-      id={id}
-      value={value}
-      onChange={handleChange}
-      required
-    />
-  </div>
+const GenericInput = ({ type, id, value, placeholder, handleChange }) => (
+  <Input
+    focus
+    placeholder={placeholder}
+    type={type}
+    className="ui focus input"
+    id={id}
+    value={value}
+    onChange={handleChange}
+    required
+  />
 );
 
 GenericInput.propTypes = {
-  label: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
