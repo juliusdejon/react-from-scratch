ad866128d1f17f107444c2dafdd6845e7ce80df5
Julius Dejon <juliusdejon@gmail.com>
2019-09-08 12:34:10 +0800

    Added semantics feed

--- src/js/HomePage/homepage.js
+++ src/js/HomePage/homepage.js
@@ -1,34 +1,112 @@
-import React, { Component } from "react";
-
+import React from "react";
+import { Feed, Icon, Header, Container } from "semantic-ui-react";
 import "./homepage.scss";
 
-import GenericInput from "../components/generic-input";
+const HomePage = () => (
+  <Container>
+    <Header>HomePage</Header>
+    <Feed>
+      <Feed.Event>
+        <Feed.Label>
+          <img src="https://react.semantic-ui.com/images/avatar/small/elliot.jpg" />
+        </Feed.Label>
+        <Feed.Content>
+          <Feed.Summary>
+            <Feed.User>Elliot Fu</Feed.User> added you as a friend
+            <Feed.Date>1 Hour Ago</Feed.Date>
+          </Feed.Summary>
+          <Feed.Meta>
+            <Feed.Like>
+              <Icon name="like" />4 Likes
+            </Feed.Like>
+          </Feed.Meta>
+        </Feed.Content>
+      </Feed.Event>
 
-class HomePage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: ""
-    };
-  }
-  handleChange(event) {
-    this.setState({ [event.target.id]: event.target.value });
-  }
-  render() {
-    const { name } = this.state;
-    return (
-      <article>
-        <form id="article-form">
-          <GenericInput
-            id="name"
-            type="text"
-            value={name}
-            placeholder={"I'm a Generic Input"}
-            handleChange={this.handleChange}
+      <Feed.Event>
+        <Feed.Label image="https://react.semantic-ui.com/images/avatar/small/helen.jpg" />
+        <Feed.Content>
+          <Feed.Summary>
+            <a>Helen Troy</a> added <a>2 new illustrations</a>
+            <Feed.Date>4 days ago</Feed.Date>
+          </Feed.Summary>
+          <Feed.Extra images>
+            <a>
+              <img src="https://react.semantic-ui.com/images/wireframe/image.png" />
+            </a>
+            <a>
+              <img src="https://react.semantic-ui.com/images/wireframe/image.png" />
+            </a>
+          </Feed.Extra>
+          <Feed.Meta>
+            <Feed.Like>
+              <Icon name="like" />1 Like
+            </Feed.Like>
+          </Feed.Meta>
+        </Feed.Content>
+      </Feed.Event>
+
+      <Feed.Event>
+        <Feed.Label image="https://react.semantic-ui.com/images/avatar/small/jenny.jpg" />
+        <Feed.Content>
+          <Feed.Summary
+            date="2 Days Ago"
+            user="Jenny Hess"
+            content="add you as a friend"
           />
-        </form>
-      </article>
-    );
-  }
-}
+          <Feed.Meta>
+            <Feed.Like>
+              <Icon name="like" />8 Likes
+            </Feed.Like>
+          </Feed.Meta>
+        </Feed.Content>
+      </Feed.Event>
+
+      <Feed.Event>
+        <Feed.Label image="https://react.semantic-ui.com/images/avatar/small/joe.jpg" />
+        <Feed.Content>
+          <Feed.Summary>
+            <a>Joe Henderson</a> posted on his page
+            <Feed.Date>3 days ago</Feed.Date>
+          </Feed.Summary>
+          <Feed.Extra text>
+            Ours is a life of constant reruns. We're always circling back to
+            where we'd we started, then starting all over again. Even if we
+            don't run extra laps that day, we surely will come back for more of
+            the same another day soon.
+          </Feed.Extra>
+          <Feed.Meta>
+            <Feed.Like>
+              <Icon name="like" />5 Likes
+            </Feed.Like>
+          </Feed.Meta>
+        </Feed.Content>
+      </Feed.Event>
+
+      <Feed.Event>
+        <Feed.Label image="https://react.semantic-ui.com/images/avatar/small/justen.jpg" />
+        <Feed.Content>
+          <Feed.Summary>
+            <a>Justen Kitsune</a> added <a>2 new photos</a> of you
+            <Feed.Date>4 days ago</Feed.Date>
+          </Feed.Summary>
+          <Feed.Extra images>
+            <a>
+              <img src="https://react.semantic-ui.com/images/wireframe/image.png" />
+            </a>
+            <a>
+              <img src="https://react.semantic-ui.com/images/wireframe/image.png" />
+            </a>
+          </Feed.Extra>
+          <Feed.Meta>
+            <Feed.Like>
+              <Icon name="like" />
+              41 Likes
+            </Feed.Like>
+          </Feed.Meta>
+        </Feed.Content>
+      </Feed.Event>
+    </Feed>
+  </Container>
+);
 export default HomePage;
