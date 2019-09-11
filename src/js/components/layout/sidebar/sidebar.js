001a1fb6f2947decd15bcfb814484679538f2d9d
Julius Dejon <juliusdejon@gmail.com>
2019-09-11 22:36:08 +0800

    Added Sidebar

---
+++ src/js/components/layout/sidebar/sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./sidebar.scss";
+
+const Sidebar = props => (
+  <nav className="sidebar">
+    <ul>
+      <li>
+        <a>Add Employee</a>
+      </li>
+      <li>
+        <a>Add Payroll</a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Sidebar;
