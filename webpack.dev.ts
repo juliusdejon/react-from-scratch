ee3493560f9dc5eeb9ad7da0eeded5fcf916a20f
Julius Dejon <juliusdejon@gmail.com>
2019-10-13 14:23:53 +0800

    Fixed importing file webpack + typecript

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -1,9 +1,6 @@
 import * as webpack from "webpack";
 import * as HtmlWebPackPlugin from "html-webpack-plugin";
-
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html"
-});
+import * as MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const config: webpack.Configuration = {
   mode: "development",
@@ -16,10 +13,35 @@ const config: webpack.Configuration = {
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: { loader: "file-loader" }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
     ]
   },
-  plugins: [htmlPlugin]
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
 };
 
 export default config;
