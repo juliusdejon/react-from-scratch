170f1ff9c6c091a20d7ffa8a8ded5a55b0b947a4
Julius Dejon <jdejon@stratpoint.com>
2020-01-21 14:18:37 +0800

    Implement Card Swipe

--- src/js/homepage/homepage.tsx
+++ src/js/homepage/homepage.tsx
@@ -1,51 +1,47 @@
-import { Breadcrumb, Typography } from "antd";
+import { Typography } from "antd";
 import * as React from "react";
-import { RotundaTable } from "../components/table";
+import { useSpring, animated } from 'react-spring'
+import { useDrag } from 'react-use-gesture'
 import "./homepage.scss";
-import { Result } from "./result/result";
-import { Search } from "./search/search";
-import { Filter } from "./filter/filter";
+
 import { Store } from "./store/store";
 
 const { Title } = Typography;
 const { useState } = React;
 
 const HomePage: React.FC = () => {
-  const [searchResult, setSearchResult] = useState(false);
+  const [isSwiped, setSwiped] = useState(false);
 
-  const toggleBreadcrumb = page => {
-    if (page === "search") {
-      setSearchResult(false);
-    } else {
-      setSearchResult(true);
+  const [{ x, y }, set] = useSpring(() => ({ x: 0, y: 0 }))
+  // Set the drag hook and define component movement based on gesture data
+  const bind = useDrag(({ down, movement: [mx, my] }) => {
+    if(down === false ) {
+      if(mx <= -135) {
+        console.log("Swiped Left")
+        setSwiped(true);
+      } 
+      if(mx >= 135) {
+        console.log("Swiped Right");
+        setSwiped(true);
+      }
     }
-  };
+    set({ x: down ? mx : 0, y: down ? my : 0 })
+  })
 
-  const search = word => {
-    console.log(word);
-    setSearchResult(true);
-  };
-
+  console.log(isSwiped);
   return (
     <article>
       <Store>
         <Title>Home</Title>
-        {/* <nav>
-          <Breadcrumb>
-            <Breadcrumb.Item onClick={() => toggleBreadcrumb("search")}>
-              Search
-            </Breadcrumb.Item>
-            <Breadcrumb.Item onClick={() => toggleBreadcrumb("result")}>
-              Result
-            </Breadcrumb.Item>
-          </Breadcrumb>
-        </nav> */}
-
-        {/* {searchResult ? <Result /> : <Search search={search} />} */}
-        <section className="container">
-          <Result />
-          <Filter />
-        </section>
+        <div className="centered-container">
+        { !isSwiped && 
+        <animated.div {...bind()} style={{ x, y }} >
+          <div className="card-container">
+              Hello, do you like coffee?
+          </div>
+        </animated.div>
+        }
+        </div>
       </Store>
     </article>
   );
